897d88ee1a9249b344b939b62592602efddd315c
Mason <Masonfashina@icloud.com>
2023-12-22 04:19:16 +0100

    added search functionality

--- js/script.js
+++ js/script.js
@@ -193,11 +193,48 @@ async function search() {
   if (global.search.term !== "" && global.search.term !== null) {
     const { results, total_pages, page_number } = await searchAPIData();
     if (results.length === 0) {
-        showAlert()
+        showAlert('No results found')
+        return
     }
+
+    displaySearchResults(results);
+
+    document.querySelector('#search-term').value ="";
   } else {
-    showAlert("please enter a search term", 'error');
+    showAlert("please enter a search term");
   }
+}
+
+function displaySearchResults(results){
+    results.forEach((result) => {
+        const div = document.createElement("div");
+        div.classList.add("card");
+        div.innerHTML = `
+        
+        <a href="${global.search.type}-details.html?id=${result.id}">
+          ${
+            result.poster_path
+              ? `<img
+            src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+            class="card-img-top"
+            alt="${global.search.type === 'movie' ? result.title:result.name}"
+          />`
+              : `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="${global.search.type === 'movie' ? result.title:result.name}"
+        />`
+          }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${global.search.type === 'movie' ? result.title:result.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_date:result.first_air_date}</small>
+          </p>
+        </div>
+        `;
+        document.querySelector("#search-results").appendChild(div);
+      });
 }
 
 //display slider Movies(step6?)
@@ -403,7 +440,7 @@ function highlightActiveLink() {
 }
 
 //show Alert
-function showAlert(message, className) {
+function showAlert(message, className = 'error') {
   const alertEl = document.createElement("div");
   alertEl.classList.add("alert", className);
   alertEl.appendChild(document.createTextNode(message));
