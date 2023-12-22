59c8a832307f57cc1801570093433e1b01bfcfe0
Mason <Masonfashina@icloud.com>
2023-12-22 01:56:48 +0100

    okay, shows added to party

--- js/script.js
+++ js/script.js
@@ -151,26 +151,120 @@ async function displayMovieDetails() {
 
 //Display backdrop on details pages
 
-function displayBackgroundImage(type, backgroundPath){
-const overlayDiv = document.createElement('div')
-overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
-overlayDiv.style.backgroundSize = 'cover';
-overlayDiv.style.backgroundPosition = 'center';
-overlayDiv.style.backgroundRepeat = 'no-repeat';
-overlayDiv.style.height = '100vh'
-overlayDiv.style.width = '100vw'
-overlayDiv.style.position = 'absolute'
-overlayDiv.style.top = '0'
-overlayDiv.style.left = '0'
-overlayDiv.style.zIndex ='-1'
-overlayDiv.style.opacity ='0.1'
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement("div");
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = "cover";
+  overlayDiv.style.backgroundPosition = "center";
+  overlayDiv.style.backgroundRepeat = "no-repeat";
+  overlayDiv.style.height = "100vh";
+  overlayDiv.style.width = "100vw";
+  overlayDiv.style.position = "absolute";
+  overlayDiv.style.top = "0";
+  overlayDiv.style.left = "0";
+  overlayDiv.style.zIndex = "-1";
+  overlayDiv.style.opacity = "0.1";
 
-if(type === 'movie'){
-    document.querySelector('#movie-details').appendChild(overlayDiv)
-}else{
-    document.querySelector('#show-details').appendChild(overlayDiv)
-
+  if (type === "movie") {
+    document.querySelector("#movie-details").appendChild(overlayDiv);
+  } else {
+    document.querySelector("#show-details").appendChild(overlayDiv);
+  }
 }
+
+//Display show Details (Step4-B)
+
+async function displayShowDetails() {
+  const showID = window.location.search.split("=")[1];
+
+  const show = await fetchAPIData(`tv/${showID}`);
+
+  //0verlay for background image
+
+  displayBackgroundImage("show", show.backdrop_path);
+
+  const div = document.createElement("div");
+
+  div.innerHTML = `
+    <div class="details-top">
+    <div>
+    ${
+      show.poster_path
+        ? `<img
+        src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+        class="card-img-top"
+        alt="${show.name}"
+      />`
+        : `<img
+      src="images/no-image.jpg"
+      class="card-img-top"
+      alt="${show.name}"
+    />`
+    }
+    </div>
+    <div>
+      <h2>${show.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${show.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+      <p>
+      ${show.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+       ${show.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+      </ul>
+      <a href="${
+        show.homepage
+      }" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Show Info</h2>
+    <ul>
+      <li><span class="text-secondary">Number of Episodes:</span>${
+        show.number_of_episodes
+      }</li>
+      <li><span class="text-secondary">Last Episode to Air:</span>${
+        show.last_episode_to_air.name
+      }</li>
+      <li><span class="text-secondary">Status:</span>${show.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+    ${show.production_companies
+      .map((company) => `<span>${company.name}</span>`)
+      .join(", ")}
+    </div>
+  </div>
+    `;
+
+  document.querySelector("#show-details").appendChild(div);
+}
+
+//Display backdrop on details pages
+
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement("div");
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = "cover";
+  overlayDiv.style.backgroundPosition = "center";
+  overlayDiv.style.backgroundRepeat = "no-repeat";
+  overlayDiv.style.height = "100vh";
+  overlayDiv.style.width = "100vw";
+  overlayDiv.style.position = "absolute";
+  overlayDiv.style.top = "0";
+  overlayDiv.style.left = "0";
+  overlayDiv.style.zIndex = "-1";
+  overlayDiv.style.opacity = "0.1";
+
+  if (type === "movie") {
+    document.querySelector("#movie-details").appendChild(overlayDiv);
+  } else {
+    document.querySelector("#show-details").appendChild(overlayDiv);
+  }
 }
 
 //fetch data from TMDB API (step2)
@@ -232,6 +326,7 @@ function init() {
       console.log("Movie details");
       break;
     case "/tv-details.html":
+      displayShowDetails();
       console.log("TV details");
       break;
     case "/search.html":
