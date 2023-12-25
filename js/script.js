98edacb9f3cf921b51c512aee643251f8e6f69bf
Mason <Masonfashina@icloud.com>
2023-12-25 16:33:02 +0100

    project complete pending updates

--- js/script.js
+++ js/script.js
@@ -192,31 +192,35 @@ async function search() {
   global.search.term = urlParams.get("search-term");
 
   if (global.search.term !== "" && global.search.term !== null) {
-    const { results, total_pages, page,total_results } = await searchAPIData();
+    const { results, total_pages, page, total_results } = await searchAPIData();
 
-      global.search.page=page;
-      global.search.totalPages=total_pages;
-      global.search.totalResults=total_results;
-
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
 
     if (results.length === 0) {
-        showAlert('No results found')
-        return
+      showAlert("No results found");
+      return;
     }
 
     displaySearchResults(results);
 
-    document.querySelector('#search-term').value ="";
+    document.querySelector("#search-term").value = "";
   } else {
     showAlert("please enter a search term");
   }
 }
 
-function displaySearchResults(results){
-    results.forEach((result) => {
-        const div = document.createElement("div");
-        div.classList.add("card");
-        div.innerHTML = `
+function displaySearchResults(results) {
+  //clear previous results
+  document.querySelector("#search-results").innerHTML = "";
+  document.querySelector("#search-results-heading").innerHTML = "";
+  document.querySelector("#pagination").innerHTML = "";
+
+  results.forEach((result) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
         
         <a href="${global.search.type}-details.html?id=${result.id}">
           ${
@@ -224,55 +228,74 @@ function displaySearchResults(results){
               ? `<img
             src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
             class="card-img-top"
-            alt="${global.search.type === 'movie' ? result.title:result.name}"
+            alt="${global.search.type === "movie" ? result.title : result.name}"
           />`
               : `<img
           src="images/no-image.jpg"
           class="card-img-top"
-          alt="${global.search.type === 'movie' ? result.title:result.name}"
+          alt="${global.search.type === "movie" ? result.title : result.name}"
         />`
           }
         </a>
         <div class="card-body">
-          <h5 class="card-title">${global.search.type === 'movie' ? result.title:result.name}</h5>
+          <h5 class="card-title">${
+            global.search.type === "movie" ? result.title : result.name
+          }</h5>
           <p class="card-text">
-            <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_date:result.first_air_date}</small>
+            <small class="text-muted">Release: ${
+              global.search.type === "movie"
+                ? result.release_date
+                : result.first_air_date
+            }</small>
           </p>
         </div>
         `;
 
-        document.querySelector('#search-results-heading').innerHTML= `
+    document.querySelector("#search-results-heading").innerHTML = `
           <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
-        `
-        document.querySelector("#search-results").appendChild(div);
-      });
+        `;
+    document.querySelector("#search-results").appendChild(div);
+  });
 
-      displayPagination()
+  displayPagination();
 }
 
 //create and pagination for search(last step)
-function displayPagination(){
-  const div = document.createElement('div');
-  div.classList.add('pagination');
+function displayPagination() {
+  const div = document.createElement("div");
+  div.classList.add("pagination");
   div.innerHTML = `
   <button class="btn btn-primary" id="prev">Prev</button>
           <button class="btn btn-primary" id="next">Next</button>
           <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
         </div>
   `;
-  document.querySelector('#pagination').appendChild(div);
+  document.querySelector("#pagination").appendChild(div);
 
   //disable prev if on first page
-  if(global.search.page === 1){
-    document.querySelector('#prev').disabled = true
+  if (global.search.page === 1) {
+    document.querySelector("#prev").disabled = true;
   }
 
   //disablenext if on last page
-  if(global.search.page === global.search.totalPages){
-    document.querySelector('#next').disabled = true
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector("#next").disabled = true;
   }
 
   //Next page
+
+  document.querySelector("#next").addEventListener("click", async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+
+  //prev page
+  document.querySelector("#prev").addEventListener("click", async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
 }
 
 //display slider Movies(step6?)
@@ -447,7 +470,7 @@ async function searchAPIData() {
   showSpinner();
 
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
   const data = await response.json();
 
@@ -478,7 +501,7 @@ function highlightActiveLink() {
 }
 
 //show Alert
-function showAlert(message, className = 'error') {
+function showAlert(message, className = "error") {
   const alertEl = document.createElement("div");
   alertEl.classList.add("alert", className);
   alertEl.appendChild(document.createTextNode(message));
